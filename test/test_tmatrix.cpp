cd6234c0564f2349e52f297d3b2d54a249b524b1
Andrey Morenkov <jedi-andr@mail.ru>
2015-11-03 02:09:07 +0300

    IT WORKS!!!!11111

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -97,7 +97,12 @@ TEST(TMatrix, can_assign_matrices_of_equal_size)
 
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(5);
+	TMatrix<int> m2(10);
+
+	ASSERT_NO_THROW(m1 = m2);
+	m1 = m2;
+	EXPECT_EQ(10, m1.GetSize());
 }
 
 TEST(TMatrix, can_assign_matrices_of_different_size)
@@ -110,9 +115,12 @@ TEST(TMatrix, can_assign_matrices_of_different_size)
 	EXPECT_EQ(m, m1);
 }
 
-TEST(TMatrix, compare_equal_matrices_return_true)
+TEST(TMatrix, compare_equal_matrices_return_true)       // как это работает????
 {
-  ADD_FAILURE();
+  	TMatrix<int> m1(5);
+	TMatrix<int> m2(5);
+
+	ASSERT_TRUE(m1 == m2);
 }
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
@@ -132,21 +140,66 @@ TEST(TMatrix, matrices_with_different_size_are_not_equal)
 
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
+    TMatrix<int> m (2);
+	TMatrix<int> m1(2);
+	TMatrix<int> m2(2);
+	TMatrix<int> m3(2);
+	
+	m [0][0]= 1;     // 1 1 
+	m [0][1]= 1;     // 0 1
+	m [1][1]= 1;
+
+	m1[0][0]= 2;     // 2 3 
+	m1[0][1]= 3;     // 0 4
+	m1[1][1]= 4;
+
+	m3[0][0]= 3;     // 3 4 
+	m3[0][1]= 4;     // 0 5
+	m3[1][1]= 5;
+
+	ASSERT_NO_THROW(m + m1);
+	m2= m + m1;
+	EXPECT_EQ(m2,m3);
 	
 }
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m (2);
+	TMatrix<int> m1(3);
+
+    ASSERT_ANY_THROW(m + m1);
 }
 
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
-  ADD_FAILURE();
+    TMatrix<int> m (2);
+	TMatrix<int> m1(2);
+	TMatrix<int> m2(2);
+	TMatrix<int> m3(2);
+	
+	m [0][0]= 2;     // 2 3 
+	m [0][1]= 3;     // 0 4
+	m [1][1]= 4;
+
+	m1[0][0]= 1;     // 1 1 
+	m1[0][1]= 1;     // 0 1
+	m1[1][1]= 1;
+
+	m3[0][0]= 1;     // 1 2 
+	m3[0][1]= 2;     // 0 3
+	m3[1][1]= 3;
+
+	ASSERT_NO_THROW(m - m1);
+	m2= m - m1;
+	EXPECT_EQ(m2,m3);
 }
 
 TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
 {
-  ADD_FAILURE();
+    TMatrix<int> m (2);
+	TMatrix<int> m1(3);
+
+    ASSERT_ANY_THROW(m - m1);
 }
 
